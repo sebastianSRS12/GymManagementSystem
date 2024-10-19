43268827a5a50fa68691b3dd5d369b30481b13b1
github_@sebastianSRS12 <sebastiansilesramires@hotmail.com>
2024-10-19 11:49:27 -0600

    Initial commit with Gym Management System files

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS  Payments;  
+DROP TABLE IF EXISTS  Attendances;  
+DROP TABLE IF EXISTS  Equipment;
+DROP TABLE IF EXISTS  Memberships;       
+DROP TABLE IF EXISTS  Members;
+DROP TABLE IF EXISTS  Clases;
+DROP TABLE IF EXISTS Instructors;
+DROP TABLE IF EXISTS Bookings;
